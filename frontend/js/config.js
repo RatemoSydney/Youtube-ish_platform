3debfb4111089c788cd78c141d1f6eb33b2f79f6
Antony Sydney Ratemo <81922580+RatemoSydney@users.noreply.github.com>
2025-08-21 14:46:39 +0300

    Create config.js

---
+++ frontend/js/config.js
@@ -0,0 +1,6 @@
+const API_BASE_URL = 'http://localhost:3000/api';
+const config = {
+  apiUrl: API_BASE_URL,
+  maxFileSize: 100 * 1024 * 1024, // 100MB
+  allowedVideoTypes: ['video/mp4', 'video/webm', 'video/quicktime', 'video/avi']
+};
